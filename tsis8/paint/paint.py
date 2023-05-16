ddd5b45b28d461c85a68b7b3b6ff99ab7b4c2784
adaialina <114148082+adaialina@users.noreply.github.com>
2023-05-16 22:00:17 +0600

    Update paint.py

--- tsis8/paint/paint.py
+++ tsis8/paint/paint.py
@@ -13,15 +13,15 @@ painting = []
 
 def draw_menu(size, color):
     pygame.draw.rect(screen, 'grey', [0, 0, WIDTH, 70]) 
-    pygame.draw.line(screen, 'black', (0, 70), (WIDTH, 70), 3)
+    pygame.draw.line(screen, 'black', (0, 70), (WIDTH, 70))
     xl_brush = pygame.draw.rect(screen, 'black', [10, 10, 50, 50])
-    pygame.draw.circle(screen, 'white', (35, 35), 20)
+    pygame.draw.circle(screen, 'white', (35, 35), (20))
     l_brush = pygame.draw.rect(screen, 'black', [70, 10, 50, 50])
-    pygame.draw.circle(screen, 'white', (95, 35), 15)
+    pygame.draw.circle(screen, 'white', (95, 35), (15))
     m_brush = pygame.draw.rect(screen, 'black', [130, 10, 50, 50])
-    pygame.draw.circle(screen, 'white', (155, 35), 10)
+    pygame.draw.circle(screen, 'white', (155, 35), (10))
     s_brush = pygame.draw.rect(screen, 'black', [190, 10, 50, 50])
-    pygame.draw.circle(screen, 'white', (215, 35), 5)
+    pygame.draw.circle(screen, 'white', (215, 35), (5))
     brushes = [xl_brush, l_brush, m_brush, s_brush]
     if size == 20:
         pygame.draw.rect(screen, 'green', [10, 10, 50, 50], 3)
@@ -32,8 +32,10 @@ def draw_menu(size, color):
     elif size == 5:
         pygame.draw.rect(screen, 'green', [190, 10, 50, 50], 3)
 
+
     pygame.draw.circle(screen, color, (400, 35), 30)
-    pygame.draw.circle(screen, 'dark gray', (400, 35), 30, 3)
+    pygame.draw.circle(screen, 'black', (400, 35), 30, 3)
+
 
     circle_btn = pygame.draw.circle(screen, 'black', (260, 24), 10, 1)
     rect_btn = pygame.draw.rect(screen, 'black', [250, 38, 20, 20], 1) 
@@ -42,7 +44,7 @@ def draw_menu(size, color):
     red = pygame.draw.rect(screen, (255, 0, 0), [WIDTH - 35, 35, 25, 25])
     green = pygame.draw.rect(screen, (0, 255, 0), [WIDTH - 60, 10, 25, 25])
     yellow = pygame.draw.rect(screen, (255, 255, 0), [WIDTH - 60, 35, 25, 25])
-    teal = pygame.draw.rect(screen, (0, 255, 255), [WIDTH - 85, 10, 25, 25])
+    teal = pygame.draw.rect(screen, (0, 255, 255), [WIDTH - 85, 35, 25, 25])
     purple = pygame.draw.rect(screen, (255, 0, 255), [WIDTH - 85, 35, 25, 25])
     white = pygame.draw.rect(screen, (0, 0, 0), [WIDTH - 110, 10, 25, 25])
     black = pygame.draw.rect(screen, (255, 255, 255), [WIDTH - 110, 35, 25, 25])
@@ -61,12 +63,15 @@ while run:
     screen.fill('white')
     mouse = pygame.mouse.get_pos()
     left_click = pygame.mouse.get_pressed()[0]
-    if left_click and mouse[1] > 70:
-        painting.append((active_color, mouse, active_size)) 
-    draw_painting(painting)
+
     if mouse[1] > 70:
         pygame.draw.circle(screen, active_color, mouse, active_size)
-    brushes, colors, rgbs = draw_menu(active_size, active_color)
+    
+    if left_click and mouse[1] > 70:
+        painting.append((active_color, mouse, active_size))
+
+    draw_painting(painting)
+    brushes, colors, rgbs = draw_menu(active_color, active_size)
 
 
     for event in pygame.event.get():
